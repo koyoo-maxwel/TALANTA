beb19a0ffde6abb62b56b67fd2677b94a004b74c
Jeff Musa <jeffmusa@gmail.com>
2018-09-19 10:35:39 +0300

    edited

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dunco/Desktop/TALANTA/virtual"
+set -gx VIRTUAL_ENV "/home/vicklyne/Talanta/TALANTA/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
